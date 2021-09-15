35799a70f5ab31c871ce7e279c1363028a618cf0
n00naki <gustavo.roch4@gmail.com>
2021-09-14 23:22:06 -0300

    :tada: Primeiro Commit

---
+++ src/test/java/br/com/timeadjust/TimeadjustApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.timeadjust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeadjustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
